2ce17873d7f623ccf28fc703c80845975e1cd5b9
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-29 17:23:10 +0200

    Datentypen int,float,double

--- Program.cs
+++ Program.cs
@@ -20,13 +20,32 @@ namespace HelloWorld
             // Console.WriteLine("Hallo Welt!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hallo Andrés!");
-            Console.ReadKey();
+            //Console.ReadKey();
+            /*
+            * Ich bin ein
+            * sehr
+            * langer Kommentar
+            **/
+            // Deklarieren und Initialisieren der Variable wert1
+            int wert1 = 25;
+            // Deklarieren
+            int wert2;
+            // Initialisieren
+            wert2 = 15;
+            int summe = wert1 + wert2;
+            // Deklarieren mehrere Variablen in einer Zeile
+            int wert3, wert4, wert5;
 
-             /*
-             * Ich bin ein
-             * sehr
-             * langer Kommentar
-             **/
+            // Verwendung von Gleitkommazahlen mit Double
+            double d1 = 333.5;
+            double d2 = 1.337;
+            double ergebnis = d1 / d2;
+
+            // Verwendung von Gleitkommazahlen mit Float
+            float f1 = 1.337f;
+
+            Console.WriteLine("Ergebnis ist " + ergebnis);
+            Console.ReadKey();
         }
     }
 }
