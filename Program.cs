6c59933f196a054698820477b5485a0e1107336e
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-29 18:37:59 +0200

    Weitere Videos über Datentypen und mehr Infos

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // Klassennamen wie z.B. ClientActivity
     class Program
     {
+        // Konstanten sind normalerweise Felder welche während
+        // der Kompilierzeit bekannt sind und danach für die gesamte Laufzeit 
+        // des Programmes nicht verändert werden können.
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        const string birthday = "31.05.1988";
+
+
+
+        // Methodenname wie z.B. CalculateValue
+        // Methodenargumente werden klein geschrieben
         // Einstiegspunkt für das Programm
         static void Main(string[] args)
         {
@@ -26,6 +38,7 @@ namespace HelloWorld
             * sehr
             * langer Kommentar
             **/
+            //--------------------------------------------------- Zahlen int, double,float
             // Deklarieren und Initialisieren der Variable wert1
             int wert1 = 25;
             // Deklarieren
@@ -46,6 +59,93 @@ namespace HelloWorld
 
             Console.WriteLine("Ergebnis ist " + ergebnis);
             Console.ReadKey();
+
+            //--------------------------------------------------- Text bzw. Strings
+            // Deklarieren und Initialisieren der Variablen meinName
+            String meinName = "Denis";
+            meinName = "Not "+ meinName;    //Waren strings nicht immutable??
+            string nachricht = "Mein Name ist " + meinName;
+            int laengeDesStrings = nachricht.Length;
+            // die ToUpper Methode gibt den String in Großschrift zurück.
+            string allesGrossGeschrieben = nachricht.ToUpper();
+            string allesKleinGeschrieben = nachricht.ToLower();
+            Console.WriteLine(allesKleinGeschrieben);
+            Console.WriteLine(allesGrossGeschrieben);
+            Console.WriteLine(nachricht + " ist {0} Zeichen lang", laengeDesStrings);
+            Console.WriteLine(nachricht + $" ist {laengeDesStrings} Zeichen lang");
+            Console.ReadKey();
+
+            //----------------------------------------------- Parsing - von String nach zahl konvertieren
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = int.Parse(myString);
+            int num2 = int.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = myString + mySecondString;
+            Console.WriteLine(resultInt);
+            Console.ReadKey();
+
+            //--------------------------------------------------- Namensgebung
+            // variable heißt wie z.B. itemCount 
+            // nutze userControl statt usrCtr
+
+            // keine Zahlen bei Variablennamen am Anfang verwenden
+            int num5 = 3;
+
+            // richtig:
+            string myName;
+            int year;
+            bool isSaved;
+
+            // vermeide:
+            // String myName;
+            // Int32 lastNum;
+            // Boolean isSaved;
+
+
+            //--------------------------------------------------- Implizite und Explizite Konvertierung
+            // Implizite Konvertierung
+            int num = 1232346;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myDouble = myFloat;
+
+            // Explizite Konvertierung
+            int myInt;
+            double myNewDouble = 13.37;
+            myInt = (int)myNewDouble;
+
+            string myString4 = myNewDouble.ToString(); // "13.37";
+            string myString2 = myString4 + "12";
+            bool sunIsShining = true;
+
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
+            Console.ReadKey();
+
+
+            //Herausforderung
+            int entero = 1;
+            bool verdadero = true;
+            float flotante = 123.456f;
+            double doblado = 654.321;
+            //... usw
+
+            string cadena1 = "Ich kontrolliere den Text";
+            string cadena2 = "45600";
+            double parseado = double.Parse(cadena2);
+            Console.WriteLine(cadena1);
+            Console.WriteLine(cadena2);
+            double triple = 3 * parseado;
+            Console.WriteLine(triple);
+
+            //--------------------------------------------------- Konstanten
+            Console.WriteLine("Mein Geburtstag ist am {0}", birthday);
+            Console.ReadKey();
+
+
         }
     }
 }
