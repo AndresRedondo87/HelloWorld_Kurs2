b50be64ce821da54db787873a459a898df4b0fde
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-29 17:16:09 +0200

    Kommentare und Farben

--- Program.cs
+++ Program.cs
@@ -8,10 +8,25 @@ namespace HelloWorld
 {
     class Program
     {
+        // Einstiegspunkt für das Programm
         static void Main(string[] args)
         {
+            /// BackgroundColor ist ein Property von Console. wir setzen ihr eine Wert aus den Enum ConsoleColor.
+            // Setzt die Hintergrundfarbe der Konsole
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Setzt die Schriftfarbe der Konsole
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            // Console.WriteLine("Hallo Welt!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo Andrés!");
             Console.ReadKey();
+
+             /*
+             * Ich bin ein
+             * sehr
+             * langer Kommentar
+             **/
         }
     }
 }
